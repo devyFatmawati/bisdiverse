1e066936e2619287035ff1ee15343f68f329f0b7
Adji Supriyono <76051851+ajspryn@users.noreply.github.com>
2023-01-18 14:28:12 +0700

    Update main.blade.php

--- Modules/Admin/Resources/views/layouts/main.blade.php
+++ Modules/Admin/Resources/views/layouts/main.blade.php
@@ -58,6 +58,33 @@
 <script src="../../../app-assets/js/scripts/forms/form-validation.js"></script>
 <!-- END: Page JS-->
 
+<script type="text/javascript">
+    // seleksi elemen video
+    var video = document.querySelector("#video-webcam");
+
+    // minta izin user
+    // navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+
+    // jika user memberikan izin
+    // if (navigator.getUserMedia) {
+        // jalankan fungsi handleVideo, dan videoError jika izin ditolak
+        // navigator.getUserMedia({ video: true }, handleVideo, videoError);
+    // }
+    
+    navigator.getUserMedia({ video: true }, handleVideo, videoError);
+
+    // fungsi ini akan dieksekusi jika  izin telah diberikan
+    function handleVideo(stream) {
+        video.srcObject = stream;
+    }
+
+    // fungsi ini akan dieksekusi kalau user menolak izin
+    function videoError(e) {
+        // do something
+        alert("Izinkan menggunakan webcam untuk demo!")
+    }
+</script>
+
 <script>
     $(window).on('load', function() {
         if (feather) {
