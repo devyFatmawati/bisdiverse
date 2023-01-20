a4199ce5ca6c825340bf3b0e3d15cc138695d800
ajspryn <adjisupriyono@gmail.com>
2023-01-20 16:38:43 +0700

    penambahan fitur bchat

--- Modules/Admin/Resources/views/layouts/main.blade.php
+++ Modules/Admin/Resources/views/layouts/main.blade.php
@@ -10,10 +10,8 @@
 <!-- BEGIN: Footer-->
 <footer class="footer footer-static footer-light">
     <p class="clearfix mb-0"><span class="float-md-start d-block d-md-inline-block mt-25">COPYRIGHT &copy;
-                2022<a class="ms-25" href="/"
-                    target="_blank">Bisnis Digital</a><span class="d-none d-sm-inline-block">, All rights
-                    Reserved</span></span><span class="float-md-end d-none d-md-block">Hand-crafted & Made with<i
-                    data-feather="heart"></i></span></p>
+            2022<a class="ms-25" href="/" target="_blank">Bisnis Digital</a><span class="d-none d-sm-inline-block">, All rights
+                Reserved</span></span><span class="float-md-end d-none d-md-block">Hand-crafted & Made with<i data-feather="heart"></i></span></p>
 </footer>
 <button class="btn btn-primary btn-icon scroll-top" type="button"><i data-feather="arrow-up"></i></button>
 <!-- END: Footer-->
@@ -57,34 +55,6 @@
 <script src="../../../app-assets/js/scripts/forms/form-tooltip-valid.min.js"></script>
 <script src="../../../app-assets/js/scripts/forms/form-validation.js"></script>
 <!-- END: Page JS-->
-
-<script type="text/javascript">
-    // seleksi elemen video
-    var video = document.querySelector("#video-webcam");
-
-    // minta izin user
-    // navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
-
-    // jika user memberikan izin
-    // if (navigator.getUserMedia) {
-        // jalankan fungsi handleVideo, dan videoError jika izin ditolak
-        // navigator.getUserMedia({ video: true }, handleVideo, videoError);
-    // }
-    
-    navigator.getUserMedia({ video: true }, handleVideo, videoError);
-
-    // fungsi ini akan dieksekusi jika  izin telah diberikan
-    function handleVideo(stream) {
-        video.srcObject = stream;
-    }
-
-    // fungsi ini akan dieksekusi kalau user menolak izin
-    function videoError(e) {
-        // do something
-        alert("Izinkan menggunakan webcam untuk demo!")
-    }
-</script>
-
 <script>
     $(window).on('load', function() {
         if (feather) {
@@ -97,15 +67,17 @@
 </script>
 <!--Start of Tawk.to Script-->
 <script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/63c6388747425128790e07d5/1gmv4pktf';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
+    var Tawk_API = Tawk_API || {},
+        Tawk_LoadStart = new Date();
+    (function() {
+        var s1 = document.createElement("script"),
+            s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/63c6388747425128790e07d5/1gmv4pktf';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+    })();
 </script>
 <!--End of Tawk.to Script-->
 </body>
--- Modules/PresensiUjian/Resources/views/index.blade.php
+++ Modules/PresensiUjian/Resources/views/index.blade.php
@@ -9,8 +9,7 @@
     <title>Presensi</title>
     <link rel="apple-touch-icon" href="../../../favicon.png">
     <link rel="shortcut icon" type="image/x-icon" href="../../../favicon.png">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
 
     <!-- BEGIN: Vendor CSS-->
     <link rel="stylesheet" type="text/css" href="../../../app-assets/vendors/css/vendors.min.css">
@@ -39,8 +38,7 @@
 
 <!-- BEGIN: Body-->
 
-<body class="vertical-layout vertical-menu-modern blank-page navbar-floating footer-static  " data-open="click"
-    data-menu="vertical-menu-modern" data-col="blank-page">
+<body class="vertical-layout vertical-menu-modern blank-page navbar-floating footer-static  " data-open="click" data-menu="vertical-menu-modern" data-col="blank-page">
     <!-- BEGIN: Content-->
     <div class="app-content content ">
         <div class="content-overlay"></div>
@@ -59,89 +57,83 @@
                             <h2 class="mb-1 text-center">Halo Mahasiswa 🚀</h2>
                             <p class="mb-3 text-center">Silahkan Tempelkan Kartu Anda Pada Alat Yang Disediakan</p>
                             <p class="mt-3 mb-1 text-center">Atau Masukan Nomor Kartu Anda</p>
-                            <form
-                                class="row row-cols-md-auto row justify-content-center align-items-center m-0 mb-2 gx-3"
-                                action="/presensiujian">
+                            <form class="row row-cols-md-auto row justify-content-center align-items-center m-0 mb-2 gx-3" action="/presensiujian">
                                 <div class="col-12 m-0 mb-1">
-                                    <input class="form-control @error('no_rfid') is-invalid @enderror" autofocus
-                                        name="no_rfid" type="text" placeholder="Mausukan Nomor Kartu Anda"
-                                        required />
+                                    <input class="form-control @error('no_rfid') is-invalid @enderror" autofocus name="no_rfid" type="text" placeholder="Masukan Nomor Kartu Anda" required />
                                 </div>
                                 <div class="col-12 d-md-block d-grid ps-md-0 ps-auto">
                                     <button class="btn btn-primary mb-1 btn-sm-block" type="submit">Absen</button>
                                 </div>
                             </form>
                             @if ($response == 'succes')
-                                <div class="card card-developer-meetup">
-                                    <div class="meetup-img-wrapper rounded-top text-center">
-                                        <img src="../../../app-assets/images/illustration/email.svg" alt="Meeting Pic"
-                                            height="170" />
-                                    </div>
-                                    <div class="card-body">
-                                        <div class="meetup-header d-flex align-items-center">
-                                            <div class="meetup-day">
-                                                <h6 class="mb-0">{{ Carbon\Carbon::parse($jadwal->tanggal_ujian)->isoformat('dddd') }}</h6>
-                                                <h3 class="mb-0">24</h3>
-                                            </div>
-                                            <div class="my-auto">
-                                                <h4 class="card-title mb-25">{{ $jadwal->matkul->nama }}</h4>
-                                                <p class="card-text mb-0">{{ $jadwal->dosen->nama }}</p>
-                                            </div>
-                                        </div>
-                                        <div class="d-flex flex-row meetings">
-                                            <div class="avatar bg-light-primary rounded me-1">
-                                                <div class="avatar-content">
-                                                    <i data-feather="user" class="avatar-icon font-medium-3"></i>
-                                                </div>
-                                            </div>
-                                            <div class="content-body">
-                                                <h6 class="mb-0">{{ $mahasiswa->nama }}</h6>
-                                                <small>{{ $mahasiswa->npm }}</small>
-                                            </div>
-                                        </div>
-                                        <div class="d-flex flex-row meetings">
-                                            <div class="avatar bg-light-primary rounded me-1">
-                                                <div class="avatar-content">
-                                                    <i data-feather="calendar" class="avatar-icon font-medium-3"></i>
-                                                </div>
-                                            </div>
-                                            <div class="content-body">
-                                                <h6 class="mb-0">Kelas {{ $mahasiswa->kelas }}</h6>
-                                                <small>Angkatan {{ $mahasiswa->tahun_masuk }}</small>
-                                            </div>
-                                        </div>
-                                        <div class="d-flex flex-row meetings">
-                                            <div class="avatar bg-light-primary rounded me-1">
-                                                <div class="avatar-content">
-                                                    <i data-feather="map-pin" class="avatar-icon font-medium-3"></i>
-                                                </div>
-                                            </div>
-                                            <div class="content-body">
-                                                <h6 class="mb-0">{{ $jadwal->ruangan }}</h6>
-                                                <small>Gedung {{ $jadwal->ruangan }}, Lantai {{ $jadwal->ruangan }}</small>
-                                            </div>
-                                        </div>
-                                    </div>
+                            <div class="card card-developer-meetup">
+                                <div class="meetup-img-wrapper rounded-top text-center">
+                                    <img src="../../../app-assets/images/illustration/email.svg" alt="Meeting Pic" height="170" />
                                 </div>
-                            @elseif($response == 'error')
-                                <div class="card card-congratulations bg-danger">
-                                    <div class="card-body text-center">
-                                        <div class="avatar avatar-xl bg-danger shadow">
+                                <div class="card-body">
+                                    <div class="meetup-header d-flex align-items-center">
+                                        <div class="meetup-day">
+                                            <h6 class="mb-0">{{ Carbon\Carbon::parse($jadwal->tanggal_ujian)->isoformat('dddd') }}</h6>
+                                            <h3 class="mb-0">24</h3>
+                                        </div>
+                                        <div class="my-auto">
+                                            <h4 class="card-title mb-25">{{ $jadwal->matkul->nama }}</h4>
+                                            <p class="card-text mb-0">{{ $jadwal->dosen->nama }}</p>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex flex-row meetings">
+                                        <div class="avatar bg-light-primary rounded me-1">
                                             <div class="avatar-content">
-                                                <i data-feather="x-circle" class="font-large-3"></i>
+                                                <i data-feather="user" class="avatar-icon font-medium-3"></i>
                                             </div>
                                         </div>
-                                        <div class="text-center">
-                                            <h1 class="mb-1 text-white">Error</h1>
-                                            <p class="card-text m-auto w-75">
-                                                {{ $title }}
-                                            </p>
+                                        <div class="content-body">
+                                            <h6 class="mb-0">{{ $mahasiswa->nama }}</h6>
+                                            <small>{{ $mahasiswa->npm }}</small>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex flex-row meetings">
+                                        <div class="avatar bg-light-primary rounded me-1">
+                                            <div class="avatar-content">
+                                                <i data-feather="calendar" class="avatar-icon font-medium-3"></i>
+                                            </div>
+                                        </div>
+                                        <div class="content-body">
+                                            <h6 class="mb-0">Kelas {{ $mahasiswa->kelas }}</h6>
+                                            <small>Angkatan {{ $mahasiswa->tahun_masuk }}</small>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex flex-row meetings">
+                                        <div class="avatar bg-light-primary rounded me-1">
+                                            <div class="avatar-content">
+                                                <i data-feather="map-pin" class="avatar-icon font-medium-3"></i>
+                                            </div>
+                                        </div>
+                                        <div class="content-body">
+                                            <h6 class="mb-0">{{ $jadwal->ruangan }}</h6>
+                                            <small>Gedung {{ $jadwal->ruangan }}, Lantai {{ $jadwal->ruangan }}</small>
                                         </div>
                                     </div>
                                 </div>
+                            </div>
+                            @elseif($response == 'error')
+                            <div class="card card-congratulations bg-danger">
+                                <div class="card-body text-center">
+                                    <div class="avatar avatar-xl bg-danger shadow">
+                                        <div class="avatar-content">
+                                            <i data-feather="x-circle" class="font-large-3"></i>
+                                        </div>
+                                    </div>
+                                    <div class="text-center">
+                                        <h1 class="mb-1 text-white">Error</h1>
+                                        <p class="card-text m-auto w-75">
+                                            {{ $title }}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
                             @else
-                                <img class="img-fluid" src="../../../app-assets/images/pages/coming-soon.svg"
-                                    alt="Coming soon page" />
+                            <img class="img-fluid" src="../../../app-assets/images/pages/coming-soon.svg" alt="Coming soon page" />
                             @endif
                         </div>
                     </div>
